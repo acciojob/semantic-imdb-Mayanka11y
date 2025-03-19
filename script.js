d4320dbf1c0d5fa38af5976683646013b9aff00b
Mayanka11y <mayankraj418890@gmail.com>
2025-03-19 10:41:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your code here
+ <p>
+            <em>Inception</em> is a science fiction thriller directed by Christopher Nolan. The movie follows Dom Cobb (Leonardo DiCaprio), a skilled thief who specializes in stealing secrets from within the subconscious during the dream state. Cobb is offered a chance to have his criminal record erased if he can successfully perform "inception" — planting an idea in someone's mind without them realizing it. As Cobb and his team delve deeper into the layers of dreams, they face increasing challenges and dangers.
+        </p>
